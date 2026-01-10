987bd89b16ba1534aaff17564c3233d373f92aad
TresorBitangimana <bitangimanatresor@gmail.com>
2026-01-09 22:43:31 -0600

    added about info

--- Portfolio2/src/About/About.jsx
+++ Portfolio2/src/About/About.jsx
@@ -7,11 +7,29 @@ function About(){
 
             <div className="about-me" id="about-me">
                 <h2 className="about-me-text" id="about-me-text">About me</h2>
-                <div className="about-info" id="about-intro">
 
+                <div className="about-info" id="about-intro">
+                    <p>
+                        Hi, my name is Tresor, I am computer science student at TCU pursuing a degree in computer science.
+                        I graduated high school in may 2025 from Chisholm Trail High School, I am very passionate about technology 
+                        and that is what made me decide to study somputer science in college.
+                    </p>
                 </div>
-                <div className="about-info"></div>
-                <div className="about-info"></div>
+                <div className="about-info" id="about-goals">
+                    <p>
+                        My goal for studying computer science is to become a Software Engineer after graduating from college, 
+                        but I do not limit myself at that, I am also interest in exploring other field in tech, I am not 100 
+                        sure but the field that interest me the most by the time I graduate will be the one.
+                    </p>
+                </div>
+
+                <div className="about-info" id="about-current">
+                    <p>
+                        At the moment I am just learning different coding languages, frames, and software. I am also exploring other 
+                        trying to explore other interest in tech, but my main goal right now is to learn what would help me become 
+                        a software engineer.
+                    </p>
+                </div>
             </div>
 
             <div className="interest" id="interest">
