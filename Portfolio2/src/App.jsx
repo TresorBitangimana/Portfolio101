aef15a4527f4fb0e8d31d82eb05a005e1487581c
TresorBitangimana <bitangimanatresor@gmail.com>
2026-01-16 16:44:56 -0600

    added media view

--- Portfolio2/src/App.jsx
+++ Portfolio2/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import "./App.css";
 
 import Profile from "./Profile/Profile.jsx";
 import Projects from "./Projects/Projects.jsx";
@@ -8,23 +8,31 @@ import Home from "./Home/Home.jsx";
 import Skills from "./Skills/Skills.jsx";
 
 function App() {
-
-  return (
-    <div className="app-container" id="app-container">
-      <div className="profile-container" id="profile-container">
-        <Profile />
-      </div>
-      <div className="content-container" id="content-contaienr">
-        <Nav />
-        <div id="home"><Home /></div>
-        <div id="about"><About /></div>
-        <div id="skills"><Skills /></div>
-        <div id="Projects"><Projects /></div>
-
-
-      </div>
-    </div>
-  )
+    return (
+        <div className="app-container" id="app-container">
+            <div className="profile-container" id="profile-container">
+                <Profile />
+            </div>
+            <div className="content-container" id="content-contaienr">
+                <Nav />
+                <div id="home">
+                    <Home />
+                </div>
+                <div id="on-page-profile">
+                    <Profile />
+                </div>
+                <div id="about">
+                    <About />
+                </div>
+                <div id="skills">
+                    <Skills />
+                </div>
+                <div id="Projects">
+                    <Projects />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
